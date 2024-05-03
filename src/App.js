67c55515c8f0c5efb56e7c44bed440554d80c9a5
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-05-04 01:49:04 +0700

    authorization and protected route, finish login and register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import './stylesheets/theme.css'
+import './stylesheets/alignments.css'
+import './stylesheets/textelements.css'
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Login from "./pages/common/Login";
+import Register from "./pages/common/Register";
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/common/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* common route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ProtectedRoute>
+          <Home/>
+        </ProtectedRoute>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
